4268d12a149272ac072016c6b1047ce098f24de1
Stephen Keable <hello@stephenkeable.co.uk>
2020-04-15 12:53:43 +0100

    eslint and es6

--- index.js
+++ index.js
@@ -1,316 +1,209 @@
-"use strict";
-
-var request = require('request');
-var qs = require('qs');
+const request = require('request');
+const qs = require('qs');
 
 /*
-
-Create instance and basic default options
-
+Internal helper functions
 */
 
-var AlliesPostcodeLookup = function () {
-    
-    if (!(this instanceof AlliesPostcodeLookup)) {
-        
-        return new AlliesPostcodeLookup();
+function sendRequest(requestUrl, callback) {
+  // Could probably get rid of dependency on the 'request' library to be honest
+
+  request(requestUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      // Convert response into a JSON object
+      const statusResponse = JSON.parse(body);
+
+      return callback(statusResponse, false);
     }
-    
-    var config = {
-        url_base: "https://ws.postcoder.com/pcw/",
-        page_num: 0,
-        options: {
-            lines: 3,
-        }
+    if (error) {
+      return callback(false, error);
+    }
+    const errorResponse = {
+      http_status: response.statusCode,
+      error_body: body,
     };
-    
-    this.config = config;
-    
+
+    return callback(false, errorResponse);
+  });
+}
+
+function trimCheck(text) {
+  if (text === false) {
+    return false;
+  }
+  if (text.trim() === '') {
+    return false;
+  }
+  return true;
 }
 
 /*
+Create instance and basic default options
+*/
 
+function AlliesPostcodeLookup() {
+  if (!(this instanceof AlliesPostcodeLookup)) {
+    return new AlliesPostcodeLookup();
+  }
+
+  const config = {
+    urlBase: 'https://ws.postcoder.com/pcw/',
+    pageNum: 0,
+    options: {
+      lines: 3,
+    },
+  };
+
+  this.config = config;
+}
+
+/*
 Init function
 
 Sets up API key (required, will default to Test key if not supplied)
 
 Optional
-
 Options object which is turned into query string parameters
 Debug use true to check search key against status service and console.log the result
-
 */
 
-AlliesPostcodeLookup.prototype.init = function (api_key, options, debug) {
-    
-    if (typeof api_key === "string") {
-        this.config.api_key = api_key;
-    } else {
-        this.config.api_key = "PCW45-12345-12345-1234X";
-    }
-    
-    var new_options = options || false;
-    
-    if (new_options) {
-        this.setOptions(new_options);
-    }
-    
-    this.config.debug = debug || false;
-    
-    if (this.config.debug === true) {
-    
-        this.checkStatus(function(response, error) {
+AlliesPostcodeLookup.prototype.init = (apiKey, options, debug) => {
+  if (typeof apiKey === 'string') {
+    this.config.apiKey = apiKey;
+  } else {
+    this.config.apiKey = 'PCW45-12345-12345-1234X';
+  }
 
-            if(error) {
-                
-                console.log(error);   
-                
-            } else {
-            
-                console.log(response);
-            }
-        });
-        
-    }
-    
+  const newOptions = options || false;
+
+  if (newOptions) {
+    this.setOptions(newOptions);
+  }
+
+  this.config.debug = debug || false;
+
+  if (this.config.debug === true) {
+    this.checkStatus((response, error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response);
+      }
+    });
+  }
 };
 
 /*
-
 Overwrite the options object after init
-
 */
 
-AlliesPostcodeLookup.prototype.setOptions = function (object) {
-   this.config.options = object; 
+AlliesPostcodeLookup.prototype.setOptions = (object) => {
+  this.config.options = object;
 };
 
 /*
-
-Overwrite the page_num
-
+Overwrite the pageNum
 */
 
-AlliesPostcodeLookup.prototype.setPageNum = function (page_num) {
-    if(typeof page_num == "number") {
-        this.config.page_num = parseInt(page_num, 10); 
-    }
+AlliesPostcodeLookup.prototype.setPageNum = (pageNum) => {
+  if (typeof pageNum === 'number') {
+    this.config.pageNum = parseInt(pageNum, 10);
+  }
 };
 
 /*
-
 Status endpoint give information about number of credits available amongst others
-
 */
 
-AlliesPostcodeLookup.prototype.checkStatus = function (callback) {
-  
-    var request_url = this.config.url_base + this.config.api_key + "/status";
-    
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-        
-    });
-    
+AlliesPostcodeLookup.prototype.checkStatus = (callback) => {
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/status`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
 
 /*
-
 Get list of countries including two letter ISO codes
-
 */
 
-AlliesPostcodeLookup.prototype.getCountries = function (callback) {
-  
-    var request_url = this.config.url_base + this.config.api_key + "/country/?" + qs.stringify(this.config.options);
-    
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-        
-    });
-    
+AlliesPostcodeLookup.prototype.getCountries = (callback) => {
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/country/?${qs.stringify(this.config.options)}`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
 
 /*
-
 International address searches
-
 */
 
-AlliesPostcodeLookup.prototype.searchAddress = function (search, country, callback) {
-    
-    var country = country || "UK";
-  
-    var page_num_str = "";
-    if(this.config.page_num > 0) {
-        var page_num_str = "&page=" + String(this.config.page_num);
-    }
-    
-    var request_url = this.config.url_base + this.config.api_key + "/address/" + country + "/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options) + page_num_str;
-    
-    send_request(request_url, function(result, error) {
+AlliesPostcodeLookup.prototype.searchAddress = (search, country, callback) => {
+  const theCountry = country || 'UK';
 
-        return callback(result, error);
-        
-    });
-    
+  let pageNumStr = '';
+  if (this.config.pageNum > 0) {
+    pageNumStr = `&page=${String(this.config.pageNum)}`;
+  }
+
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/address/${theCountry}/${encodeURIComponent(search)}?${qs.stringify(this.config.options)}${pageNumStr}`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
 
-AlliesPostcodeLookup.prototype.searchAddressGeo = function (search, country, callback) {
-  
-    var country = country || "UK";
-    
-    var page_num_str = "";
-    if(this.config.page_num > 0) {
-        page_num_str = "&page=" + String(this.config.page_num);
-    }
-    
-    var request_url = this.config.url_base + this.config.api_key + "/addressgeo/" + country + "/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options) + page_num_str;
-    
-    send_request(request_url, function(result, error) {
+AlliesPostcodeLookup.prototype.searchAddressGeo = (search, country, callback) => {
+  const theCountry = country || 'UK';
 
-        return callback(result, error);
-        
-    });
-    
+  let pageNumStr = '';
+  if (this.config.pageNum > 0) {
+    pageNumStr = `&page=${String(this.config.pageNum)}`;
+  }
+
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/addressgeo/${theCountry}/${encodeURIComponent(search)}?${qs.stringify(this.config.options)}${pageNumStr}`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
 
-AlliesPostcodeLookup.prototype.getSingleAddress = function (udprn, country, callback) {
-  
-    var udprn = udprn || false;
-    var country = country || "UK";
-    
-    if(trim_check(udprn) !== false) {
-  
-        var request_url = this.config.url_base + this.config.api_key + "/address/" + country + "/udprn?udprn=" + encodeURIComponent(udprn) + "&" + qs.stringify(this.config.options);
+AlliesPostcodeLookup.prototype.getSingleAddress = (udprn, country, callback) => {
+  const theUdprn = udprn || false;
+  const theCountry = country || 'UK';
 
-        send_request(request_url, function(result, error) {
+  if (trimCheck(theUdprn) !== false) {
+    const requestUrl = `${this.config.urlBase}${this.config.apiKey}/address/${theCountry}/udprn?udprn=${encodeURIComponent(theUdprn)}&${qs.stringify(this.config.options)}`;
+    sendRequest(requestUrl, (result, error) => callback(result, error));
+    return true;
+  }
+  const errorResponse = {
+    http_status: 404,
+    error_body: 'No UDPRN parameter supplied',
+  };
 
-            return callback(result, error);
-
-        });
-        
-    } else {
-        
-        var error_response = {
-            http_status: 404,
-            error_body: "No UDPRN parameter supplied"
-        };
-        
-        return callback(false, error_response);
-    }
-    
+  return callback(false, errorResponse);
 };
 
-AlliesPostcodeLookup.prototype.getSingleAddressGeo = function (udprn, country, callback) {
-  
-    var udprn = udprn || false;
-    var country = country || "UK";
-    
-    if (trim_check(udprn) !== false) {
-  
-        var request_url = this.config.url_base + this.config.api_key + "/addressgeo/" + country + "/udprn?udprn=" + encodeURIComponent(udprn) + "&" + qs.stringify(this.config.options);
+AlliesPostcodeLookup.prototype.getSingleAddressGeo = (udprn, country, callback) => {
+  const theUdprn = udprn || false;
+  const theCountry = country || 'UK';
 
-        send_request(request_url, function(result, error) {
+  if (trimCheck(theUdprn) !== false) {
+    const requestUrl = `${this.config.urlBase}${this.config.apiKey}/addressgeo/${theCountry}/udprn?udprn=${encodeURIComponent(theUdprn)}&${qs.stringify(this.config.options)}`;
+    sendRequest(requestUrl, (result, error) => callback(result, error));
+    return true;
+  }
+  const errorResponse = {
+    http_status: 404,
+    error_body: 'No UDPRN parameter supplied',
+  };
 
-            return callback(result, error);
-
-        });
-        
-    } else {
-        
-        var error_response = {
-            http_status: 404,
-            error_body: "No UDPRN parameter supplied"
-        };
-        
-        return callback(false, error_response);
-    }
-    
+  return callback(false, errorResponse);
 };
 
 /*
-
-UK only street level searches, do not include premise level details (organisation names, building names or numbers)
-
+UK only street level searches, do not include premise level details
+(organisation names, building names or numbers)
 */
 
-AlliesPostcodeLookup.prototype.searchStreet = function (search, callback) {
-  
-    var request_url = this.config.url_base + this.config.api_key + "/street/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-    
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-        
-    });
-    
+AlliesPostcodeLookup.prototype.searchStreet = (search, callback) => {
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/street/UK/${encodeURIComponent(search)}?${qs.stringify(this.config.options)}`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
 
-AlliesPostcodeLookup.prototype.searchStreetGeo = function (search, callback) {
-  
-    var request_url = this.config.url_base + this.config.api_key + "/geo/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-    
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-        
-    });
-    
+AlliesPostcodeLookup.prototype.searchStreetGeo = (search, callback) => {
+  const requestUrl = `${this.config.urlBase}${this.config.apiKey}/geo/UK/${encodeURIComponent(search)}?${qs.stringify(this.config.options)}`;
+  sendRequest(requestUrl, (result, error) => callback(result, error));
 };
-
-/* 
-
-Internal helper functions
-
-*/
-
-function send_request(request_url, callback) {
-    
-    // Could probably get rid of dependency on the 'request' library to be honest
-    
-    request(request_url, function (error, response, body) {
-
-        if (!error && response.statusCode == 200) {
-
-            // Convert response into a JSON object
-            var status_response = JSON.parse(body);
-            
-            return callback(status_response, false);
-            
-        } else {
-            if (error) {
-            
-                return callback(false, error);
-                
-            } else {
-                
-                var error_response = {
-                    http_status: response.statusCode,
-                    error_body: body
-                };
-            
-                return callback(false, error_response);
-            }
-
-        }
-
-    });
-    
-}
-
-function trim_check(text) {
-    
-    if(text === false) {
-        return false;
-    } else if (text.trim() == "") {
-        return false;
-    } else {
-        return true
-    }
-    
-}
 
 module.exports = new AlliesPostcodeLookup();
